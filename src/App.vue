9e0defd78dc35fe02b1f516b97e8bf0150654dfc
Marcsu <marcsu@pop-os.localdomain>
2019-07-18 16:33:48 +0100

    pwa  firebase

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <qrcode-stream @init="onInit" @decode="onDecode"></qrcode-stream>
+    <div v-for="item in list">
+      {{item}} <button v-clipboard="item"> Copy </button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { QrcodeStream } from 'vue-qrcode-reader'
+import Clipboard from 'v-clipboard'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    QrcodeStream,
+    Clipboard,
+  },
+  data: () => ({
+    list:[],
+  }),
+  methods: {
+    onDecode(decodedString) {
+      console.log(decodedString);
+      this.list.push(decodedString)
+    },
+    async onInit(promise) {
+    // show loading indicator
+
+      try {
+        await promise;
+
+        // successfully initialized
+      } catch (error) {
+        if (error.name === "NotAllowedError") {
+          // user denied camera access permisson
+        } else if (error.name === "NotFoundError") {
+          // no suitable camera device installed
+        } else if (error.name === "NotSupportedError") {
+          // page is not served over HTTPS (or localhost)
+        } else if (error.name === "NotReadableError") {
+          // maybe camera is already in use
+        } else if (error.name === "OverconstrainedError") {
+          // did you requested the front camera although there is none?
+        } else if (error.name === "StreamApiNotSupportedError") {
+          // browser seems to be lacking features
+        }
+      } finally {
+        // hide loading indicator
+      }
+    }
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
